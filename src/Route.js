e7956e5c8ad5e8265c26aed2d82511af275f11bb
undisputed679 <mayankbhushan679@gmail.com>
2021-08-20 22:42:12 +0530

    todo app

---
+++ src/Route.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import App from './components/App'
+
+function Route() {
+  return (
+    <div className="App">
+      <App/>
+    </div>
+  );
+}
+
+export default Route;
---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+  import React from "react";
+  import './app.css'
+function App() {
+	const [item, setItem] = React.useState("");
+	const [todoList, setTodoList] = React.useState([]);
+
+	function handleChange(event) {
+		setItem(event.target.value);
+	}
+
+	function handleSubmit(event) {
+		setTodoList((prevValue) => [...prevValue, item]);
+		setItem("");
+		event.preventDefault();
+	}
+
+	return (
+		<div className="container">
+			<div className="heading">
+				<h1>To-Do List</h1>
+			</div>
+			<div className="form">
+				<form onSubmit={handleSubmit}>
+					<input onChange={handleChange} type="text" value={item} />
+					<button type="submit">
+						<span>Add</span>
+					</button>
+				</form>
+			</div>
+			<div>
+				<ul>
+					{todoList.map((todo, index) => (
+						<li key={index}>{todo}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default App;
